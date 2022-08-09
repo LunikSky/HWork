1315f05946bc73fce0f93039f03ef2d6c271ae8e
LunikSky <110670782+LunikSky@users.noreply.github.com>
2022-08-09 19:29:48 +1000

    Update Test1Easy.cs

--- Test1Easy.cs
+++ Test1Easy.cs
@@ -2,15 +2,9 @@
 
 public class Test1Easy
 {
-	public void CalculatingAreaTriangle()
+	public double CalculatingAreaTriangle(double a, double b)
 	{
-        Console.WriteLine("Введите длинну катета: a");
-        double a = Convert.ToDouble(Console.ReadLine());
-
-        Console.WriteLine("Введите длинну катета: b");
-        double b = Convert.ToDouble(Console.ReadLine());
-
-        Console.WriteLine($"Площадь прямоугольного треугольника равна: {(a * b) / 2}");
-    }
+        return ($"Площадь прямоугольного треугольника равна: {(a * b) / 2}");
+        }
 }
 
